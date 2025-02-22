e609c9e02f5529f7e1b3c2e435c624ed59d52d78
alunoEtec <aula@etec.com>
2025-02-21 22:28:51 -0300

    Aula 21 02

--- index.php
+++ index.php
@@ -6,91 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROJETO PWIIB </title>
     <link href="bootstrap.min.css" rel="stylesheet" >
-    <style>
-      .row{
-        border:2px solid red;
-        padding:5px
-      }
-      .row> div {
-        border:2px solid blue;
-        padding:50px 0;
-        text-align:center
-      }
-      </style>
-</head>
-<body>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+   
+ 
+<nav class="navbar navbar-expand-lg  bg-primary " data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Projeto pwiiB</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#"></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#"></a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            PHP
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">variavel</a></li>
+            <li><a class="dropdown-item" href="#">if</a></li>
+            <li><a class="dropdown-item" href="#">while</li>
+            <li><a class="dropdown-item" href="#">For</a></li>
+            <li><a class="dropdown-item" href="#">Switch</a></li>
+            <li><a class="dropdown-item" href="#">Array/Vetor</a></li>  
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link disabled" aria-disabled="true"></a>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -101,82 +47,12 @@
   </div>
 </nav>
 
-<h1> Sistema de Layout</h1>
-    Featured
-  
-</div>
-    
-    <div class="row">
-      <div class="col-6">
-        Cadastrar
-    </div>
-    <div class="col-6">
-      Pesquisar
-    </div>
-    <div class="row">
-  
-        <div class="col-4">
-        <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-  <label for="floatingInput">Email cadastrado</label>
-</div>
-<div class="form-floating">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
-</div>
-          
-    </div>
-    <div class="col-4">
-    </div>
-    <div class="col-4">
-    </div>
+
       
 
 
     
-    <a href= "contato.php"> Pagina de inicial  </a>
-    <?php
-    echo"<h1>Titulo do Projeto PWBII B</h1>";
-    ?>
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Deseja sair ? </h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        .marli 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-   Menu de opçoes 
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="www.globo.com" acessar globo.com </a></li>
-    <li><a class="www.uol.com" acessar.uol.com </a></li>
-    <li><a class="www.record.com" acessar record.com </a></li>
-  </ul>
-</div>
-
-
-
-    <script src="bootstrap.bundle.min.js" ></script>
- 
+   <script src="bootstrap.bundle.min.js"></script>
 </body>
 </html>
 
