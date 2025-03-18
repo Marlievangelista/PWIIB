6f4757253fb0128dc6a56f6eafe1943e4a8a6ffe
alunoEtec <aula@etec.com>
2025-03-18 20:47:33 -0300

    aulas atualizadas

---
+++ formulario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <h1>Cadastro de Usuario</h1>
+        <form action="salvar_dados.php" method= "post">
+        <label>Email</label>
+        <input type="text" name="login" value=""/>
+        <br/>
+        <label>Senha</label>
+        <input type="password" name="senha" value=""/>
+        <br/>
+        <button type="submit">
+            Logar
+           </button>
+
+           <nav>
+
+        <h1>Formulario</h1>
+        <form action="salvar_dados.php" method= "GET">
+        <label>Nome</label>
+        <input type="text" name="nome" value=""/>
+        <br/>
+        <label>Idade</label>
+        <input type="" name="idade " value=""/>
+        <br/>
+        <button type="submit">
+            Logar
+           </button>
+
+
+</form>
+<script src="boostrap.bundle.min.js"></script>
+</body>
+
+</body>
+</html>
---
+++ salvar_dados.php
@@ -0,0 +1,19 @@
+<?php
+echo $_GET ["login"];
+echo "<br/>";
+echo $_GET ["senha"];
+
+echo "<br>";
+
+echo $_POST ["login"];
+echo "<br/>";
+echo $_POST ["senha"];
+echo"<br>";
+
+echo $_GET ["nome"];
+echo "<br/>";
+echo $_GET ["idade"];
+
+
+
+?>
