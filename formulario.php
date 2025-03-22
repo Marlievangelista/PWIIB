00fb9374efb519de21ec7dda4d72b8f97ffc5947
alunoEtec <aula@etec.com>
2025-03-21 22:35:54 -0300

    Em execução

--- formulario.php
+++ formulario.php
@@ -28,12 +28,42 @@
         <input type="text" name="nome" value=""/>
         <br/>
         <label>Idade</label>
-        <input type="" name="idade " value=""/>
+        <input type="idade" name="idade " value=""/>
         <br/>
         <button type="submit">
             Logar
            </button>
 
+           <nav>
+
+           <h1>Formulario</h1>
+        <form action="salvar_dados.php" method= "GET">
+        <label for>Nome : Digite seu nome :</label>
+        <input type="text" id="nome" name="nome" required>
+        <br/>
+        <label>Ola!</label>
+        <input type="Ola" name="nome" value=""/>
+        
+        <button type="submit">Enviar</button>
+            
+           </button>
+           <nav>
+
+           <h1>Cadastro de Usuario</h1>
+        <form action="salvar_dados.php" method= "post">
+        <label>login</label>
+        <input type="text" name="login" value=""/>
+        <br/>
+        <label>nome </label>
+        <input type="text" name="nome" value=""/>
+        <br/>
+        <label>senha </label>
+        <input type="password" name="senha" value=""/>
+        <button type="submit">
+            Logar
+           </button>
+
+              
 
 </form>
 <script src="boostrap.bundle.min.js"></script>
--- salvar_dados.php
+++ salvar_dados.php
@@ -10,10 +10,21 @@ echo "<br/>";
 echo $_POST ["senha"];
 echo"<br>";
 
+
+
 echo $_GET ["nome"];
 echo "<br/>";
 echo $_GET ["idade"];
 
+echo "<br>";
 
 
-?>
+        if (isset($_GET['nome'])) {
+            $nome = htmlspecialchars($_GET['nome']);
+            echo "<h2>Olá, $nome!</h2>";
+        }
+    ?>
+ <br>
+
+
+
